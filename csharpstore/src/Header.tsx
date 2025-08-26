9008ab290334420067c83be02eae06f77477ecde
pedrocpdev1 <pedrocpdev@gmail.com>
2025-08-26 13:41:25 -0300

    Tailwind initial implementation

--- csharpstore/src/Header.tsx
+++ csharpstore/src/Header.tsx
@@ -1,17 +1,23 @@
-import './App.css'
+import "./App.css";
 
-function Header(){
-    return (
-        <div className='Header' >
-            <header className='App-header'>
-                <nav >
-                    <a href="">Home</a>
-                    <a href="">Produtos</a>
-                    <a href="">Sobre nós</a>
-                </nav>
-            </header>
-        </div>
-    )
+function Header() {
+  return (
+    <div className="Header">
+      <header className="App-header">
+        <nav className="flex items-center justify-between p-4 bg-blue-800 text-white">
+          <img src="images/shopping-basket.png" alt="Logo" className="h-7 invert" />
+          <div className="space-x-5 flex">
+            <a href="" className="hover:text-black">Home</a>
+            <a href=""className="hover:text-black">Produtos</a>
+            <a href=""className="hover:text-black">Sobre nós</a>
+            <a href="" className="hover:invert">
+                <img src="/images/cardapio.png" className="h-7 invert " />
+            </a>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 export default Header;
