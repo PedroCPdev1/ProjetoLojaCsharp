2b153257c5030a537c1fd604800d4b674c1a5a09
pedrocpdev1 <pedrocpdev@gmail.com>
2025-08-26 11:35:19 -0300

    Tailwind React Vite install

---
+++ csharpstore/src/Body.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function Body() {
+    return (
+        <h1>teste</h1>
+    )
+}
+
+export default Body;
---
+++ csharpstore/src/Header.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function Header(){
+    return (
+        <div className='Header' >
+            <header className='App-header'>
+                <nav >
+                    <a href="">Home</a>
+                    <a href="">Produtos</a>
+                    <a href="">Sobre nós</a>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
