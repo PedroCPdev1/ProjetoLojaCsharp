8ad96c8d9019959aa8222dd0100e6fbf838994be
pedrocpdev1 <pedrocpdev@gmail.com>
2025-08-27 23:35:51 -0300

    Adding color transition delay

--- csharpstore/src/jsx/Header.tsx
+++ csharpstore/src/jsx/Header.tsx
@@ -7,11 +7,11 @@ function Header() {
         <nav className="flex items-center justify-between p-4 bg-cyan-700 text-white">
           <img src="images/shopping-basket.png" alt="Logo" className="h-7 invert" />
           <div className="space-x-5 flex">
-            <a href="" className="hover:text-black">Home</a>
-            <a href=""className="hover:text-black">Produtos</a>
-            <a href=""className="hover:text-black">Sobre nós</a>
+            <a href="" className="hover:text-black transition-colors duration-200">Home</a>
+            <a href=""className="hover:text-black transition-colors duration-200">Produtos</a>
+            <a href=""className="hover:text-black transition-colors duration-200">Sobre nós</a>
             <a href="" className="hover:invert">
-                <img src="/images/cardapio.png" className="h-7 invert " />
+            <img src="/images/cardapio.png" className="h-7 invert transition-colors duration-200" />
             </a>
           </div>
         </nav>
